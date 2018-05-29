701f0325a4f9ac73c2eeb375fdb4bbbb990a5cd3
dialv <dialvultimate@gmail.com>
2018-05-29 01:12:16 -0600

    primer comit

--- README.md
+++
@@ -1,2 +0,0 @@
-# SISADMRH
-sistema de administracion de recursos humanos mjsp
---
+++ src/main/java/com/sisadmrh/sisadmrh/MjspApplication.java
@@ -0,0 +1,12 @@
+package com.sisadmrh.sisadmrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjspApplication.class, args);
+	}
+}
