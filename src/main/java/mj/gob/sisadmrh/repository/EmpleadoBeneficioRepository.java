d48c39ce6ec6448ae6b4d79984026501ccae1a99
dialv <dialvultimate@gmail.com>
2018-06-25 02:29:20 -0600

    nuevos objetos para reporte

---
+++ src/main/java/mj/gob/sisadmrh/repository/EmpleadoBeneficioRepository.java
@@ -0,0 +1,7 @@
+package mj.gob.sisadmrh.repository;
+
+import mj.gob.sisadmrh.model.Beneficio;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BeneficioRepository extends CrudRepository<Beneficio, Integer>{
+}
