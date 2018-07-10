446eadfaa343954b145cde2265d82e94cf81fbdd
dialv <dialvultimate@gmail.com>
2018-07-10 02:42:42 -0600

    agregando capa de seguridad

--- src/main/java/mj/gob/sisadmrh/controller/HomeController.java
+++ src/main/java/mj/gob/sisadmrh/controller/HomeController.java
@@ -1,33 +1,13 @@
 package mj.gob.sisadmrh.controller;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRExporterParameter;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.export.JRPdfExporter;
-import net.sf.jasperreports.engine.export.JRPdfExporterParameter;
-import net.sf.jasperreports.engine.util.JRLoader;
-import org.apache.commons.io.IOUtils;
+import java.security.Principal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /**
  *
@@ -35,8 +15,76 @@ import org.springframework.web.context.request.ServletRequestAttributes;
  */
 @Controller
 public class HomeController {
-   @RequestMapping("/")
-    public String index() {
-        return "index";
+//   @RequestMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+    
+    @RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
+    public String welcomePage(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "Bienvenido");
+        return "welcomePage";
+    }
+ 
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(Model model, Principal principal) {
+         
+        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+ 
+        String userInfo = WebUtils.toString(loginedUser);
+        model.addAttribute("userInfo", userInfo);
+         
+        return "adminPage";
+    }
+ 
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(Model model) {
+ 
+        return "loginPage";
+    }
+ 
+    @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
+    public String logoutSuccessfulPage(Model model) {
+        model.addAttribute("title", "Logout");
+        return "logoutSuccessfulPage";
+    }
+ 
+    @RequestMapping(value = "/userInfo", method = RequestMethod.GET)
+    public String userInfo(Model model, Principal principal) {
+ 
+        // (1) (en)
+        // After user login successfully.
+        // (vi)
+        // Sau khi user login thanh cong se co principal
+        String userName = principal.getName();
+ 
+        System.out.println("User Name: " + userName);
+ 
+        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+ 
+        String userInfo = WebUtils.toString(loginedUser);
+        model.addAttribute("userInfo", userInfo);
+ 
+        return "userInfoPage";
+    }
+ 
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public String accessDenied(Model model, Principal principal) {
+ 
+        if (principal != null) {
+            User loginedUser = (User) ((Authentication) principal).getPrincipal();
+ 
+            String userInfo = WebUtils.toString(loginedUser);
+ 
+            model.addAttribute("userInfo", userInfo);
+ 
+            String message = "Hola " + principal.getName() //
+                    + "<br> Tu no cuentas con los permisos para acceder a esta pagina por favor consulta con tu administrador!";
+            model.addAttribute("message", message);
+ 
+        }
+ 
+        return "403Page";
     }
 }
--- src/main/java/mj/gob/sisadmrh/repository/UsuarioRepository.java
+++ src/main/java/mj/gob/sisadmrh/repository/UsuarioRepository.java
@@ -1,7 +1,11 @@
 package mj.gob.sisadmrh.repository;
 
 import mj.gob.sisadmrh.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer>{
+    @Query("SELECT o FROM Usuario o WHERE o.nombreusuario=?1")
+    public Usuario findbyusername(String username);
+
 }
