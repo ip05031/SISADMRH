01c3008603e63a1e42c4f8411eb11062ab746253
j <jorgealbertocardoza@gmail.com>
2018-08-28 11:10:05 -0600

    inasistencia

---
+++ src/main/java/mj/gob/sisadmrh/controller/Inasistencia/InasistenciaController.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mj.gob.sisadmrh.controller.Inasistencia;
+
+import mj.gob.sisadmrh.controller.UtilsController;
+import mj.gob.sisadmrh.model.Empleado;
+import mj.gob.sisadmrh.model.Inasistencia;
+import mj.gob.sisadmrh.service.EmpleadoService;
+import mj.gob.sisadmrh.service.InasistenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author jorge
+ */
+@Controller
+@RequestMapping(value = "inasistencias")
+public class InasistenciaController extends UtilsController{
+    @Autowired
+    private EmpleadoService empleadoService;
+    @Autowired
+    private InasistenciaService inasistenciaService;
+    
+    
+    private final String PREFIX = "fragments/inasistencia/";
+      @RequestMapping(value = "/", method=RequestMethod.GET)
+    public String list(Model model){
+    model.addAttribute("inasistencias", inasistenciaService.listAllInasistencia());
+    return PREFIX + "inasistencias";
+    }
+       @RequestMapping("edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("inasistencia",inasistenciaService.getInasistenciaById(id));
+         return PREFIX + "inasistenciaform";
+    }
+    
+     @RequestMapping("new/inasistencia")
+    public String newInasistencia(Model model) {
+        model.addAttribute("inasistencia", new Inasistencia());
+          Iterable<Empleado> empleados = empleadoService.listAllEmpleado();
+//         
+      model.addAttribute("empleados", empleados);
+       return PREFIX + "inasistenciaform";
+                }
+    
+    
+       @RequestMapping(value = "inasistencia")
+    public String saveInasistencia(Inasistencia inasistencia,Model model) {
+        try{
+        inasistenciaService.saveInasistencia(inasistencia);
+        
+        model.addAttribute("msg", 0);
+       // model.addAttribute("inasistencias", inasistenciaService.listAllInasistencia());
+      // return PREFIX+"inasistencias";
+        }
+        catch(Exception e){
+          model.addAttribute("msg", 1);
+        }
+        // return PREFIX+"inasistenciaform";
+        return PREFIX+"inasistencias";
+       // return "redirect:./show/" + comision.getCodigocomision();
+    }
+     @RequestMapping("show/{id}")
+    public String Inasistencia(@PathVariable Integer id, Model model) {
+        model.addAttribute("inasistencia", inasistenciaService.getInasistenciaById(id).get());
+        return PREFIX +"inasistenciashow";
+    }
+    
+    @RequestMapping("delete/{id}")
+    public String delete(@PathVariable Integer id,Model model) {
+        try{
+        inasistenciaService.deleteInasistencia(id);
+         model.addAttribute("msg", 3);
+        }
+        catch(Exception e){
+        model.addAttribute("msg", 4);
+         
+        }
+         return PREFIX + "inasistencias";
+        //return "redirect:/comisiones/";
+    }
+    
+}
---
+++ src/main/java/mj/gob/sisadmrh/controller/capacitacion/CapacitacionController.java
@@ -0,0 +1,156 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mj.gob.sisadmrh.controller.capacitacion;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import javax.servlet.http.HttpServletResponse;
+import mj.gob.sisadmrh.controller.UtilsController;
+import mj.gob.sisadmrh.model.Capacitacion;
+import mj.gob.sisadmrh.model.Capacitador;
+import mj.gob.sisadmrh.service.CapacitacionService;
+import mj.gob.sisadmrh.service.CapacitadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "capacitaciones")
+public class CapacitacionController extends UtilsController{
+    
+    
+    private CapacitacionService capacitacionService;
+     
+       @Autowired
+    public void setCapacitacionService(CapacitacionService capacitacionService) {
+        this.capacitacionService= capacitacionService;
+    }
+    
+    
+    private CapacitadorService capacitadorService;
+    @Autowired
+    public void SetCapacitadorService(CapacitadorService capacitadorService){
+    this.capacitadorService=capacitadorService;
+    }
+    
+  
+    private final String PREFIX = "fragments/capacitacion/";
+    @RequestMapping(value = "/", method=RequestMethod.GET)
+    public String list(Model model){
+        model.addAttribute("capacitaciones", capacitacionService.listAllCapacitacion());
+        return PREFIX + "capacitaciones";
+    }
+    
+     @RequestMapping("edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("capacitacion", capacitacionService.getCapacitacionById(id));
+        return PREFIX + "capacitacionform";
+    }
+    
+    @RequestMapping("new/capacitacion")
+    public String newCapacitacion(Model model) {
+        model.addAttribute("capacitacion", new Capacitacion());
+        
+          Iterable<Capacitador> capacitadores = capacitadorService.listAllCapacitador();
+         // System.out.println("numero:"+capacitadores);
+        model.addAttribute("capacitadores", capacitadores);
+        return PREFIX + "capacitacionform";
+    }
+    
+    @RequestMapping(value = "capacitacion")
+    public String saveCapacitacion(Capacitacion capacitacion,Model model) {
+        try{
+           capacitacionService.saveCapacitacion(capacitacion);
+           model.addAttribute("msg", 0);
+        }
+        catch(Exception e){
+           model.addAttribute("msg", 1);
+        }
+       return PREFIX + "capacitacionform";
+       
+        //return "redirect:./show/" + capacitacion.getCodigocapacitacion();
+    }
+    
+//      @RequestMapping(value = "comite",method=RequestMethod.POST)
+//    public String saveComite(@Valid @ModelAttribute(name = "comite") Comite comite) {
+//        comiteService.saveComite(comite);
+//       
+//        return "redirect:./show/" + comite.getCodigocomite();
+//    }
+//    
+//    
+     @RequestMapping("show/{id}")
+    public String showCapacitacion(@PathVariable Integer id, Model model) {
+        
+        model.addAttribute("capacitacion", capacitacionService.getCapacitacionById(id).get());
+        
+        return PREFIX +"capacitacionshow";
+    }
+     @RequestMapping("delete/{id}")
+    public String delete(@PathVariable Integer id,Model model) {
+        try{
+       
+        capacitacionService.deleteCapacitacion(id);
+         model.addAttribute("msg", 3);
+        }
+        catch(Exception e)
+        {
+        model.addAttribute("msg", 4);
+        }
+   
+        return "redirect:/capacitaciones/";
+    }
+    
+    @RequestMapping("report/")
+    public String reporte() {
+        return PREFIX + "capacitacionesreport";
+    }
+    
+    @RequestMapping(value = "pdf/{indice}", method = { RequestMethod.POST, RequestMethod.GET })
+    public void pdf(@PathVariable("indice") Long indice, 
+            @RequestParam(required = false) Boolean download, 
+            @RequestParam(value="fechainicial",required = false) String fechainicio, 
+            @RequestParam(value="fechafinal", required = false) String fechafin, 
+                HttpServletResponse response) throws Exception {
+                Map<String, Object> params = new HashMap<>();
+		params.put("CODIGO", indice.toString());
+		params.put("FECHAINICIO", fechainicio);
+		params.put("FECHAFIN", fechafin);
+        	generatePdf("capacitaciones", "rpt_capacitaciones", params, download,response);
+    }
+    
+    
+    
+      @RequestMapping("buscar/")
+    public String buscar() {
+             
+        return PREFIX +"buscar";
+    }
+    
+    
+      @RequestMapping(value="buscar/listar/{dato}",method = { RequestMethod.GET})
+    public ModelAndView listCapacitacion(@PathVariable("dato") String dato) {
+        
+          ModelAndView mv = new ModelAndView(PREFIX +"listCapacitacion");
+          
+       Iterable<Capacitacion> lista =  capacitacionService.findByDato(dato);
+          
+          
+           mv.addObject("capacitaciones", lista);
+           mv.addObject("dato", dato);
+        return mv;
+    }
+    
+    
+}
